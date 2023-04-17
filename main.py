651e9949889217dd4ac138d2804173b214cbc415
Виктор <lenkov.victor@yandex.ru>
2023-04-17 22:19:40 +0300

    111

--- main.py
+++ main.py
@@ -68,31 +68,8 @@ def ScrabbleGame():
 ###################################################
 
 def StudentsAndForeignLanguages():
-    students = {"Иванов", "Петров", "Смирнов", "Сидоров", "Васильев", "Кузнецов", "Попов", "Федоров", "Лебедев", "Семенов"}
-    languages = {"Русский", "Английский", "Французский", "Немецкий", "Китайский"}
-
-    lang_stud = {}
-
-    for st in students:
-        kolvo = random.randint(1, 3)
-        lang_stud[st] = random.sample(list(languages), kolvo)
-
-    unique_lang = set()
-    for i in lang_stud:
-        unique_lang = unique_lang.union(set(lang_stud[i]))
-
-    # print(lang_stud)
-    print("Множество различных языков, которые знают студенты: ", sorted(unique_lang), f" ({len(unique_lang)})")
-
-    china = [i for i in lang_stud if "Китайский" in lang_stud[i]]
-    print("Студенты, знающие китайский: ", china)
-
-
-
-
-
-
     languages = ["Русский", "Английский", "Французский", "Немецкий", "Китайский", "Японский", "Корейский", "Испанский", "Польский", "Чешский"]
+    languagesNum = ["0-Русский", "1-Английский", "2-Французский", "3-Немецкий", "4-Китайский", "5-Японский", "6-Корейский", "7-Испанский", "8-Польский", "9-Чешский"]
     students = []
     print('Введите фамилии студентов (вводятся через ENTER, для прекращения ввода введите STOP).')
     BreakOutFlag = False
@@ -112,7 +89,7 @@ def StudentsAndForeignLanguages():
     print(students)
     print('Внесите информацию по владению языками каждым из студентов (Введите номера языков из списка, начиная с 0).')
     print('Список доступных языков:')
-    print(languages)
+    print(languagesNum)
     StudentsLanguages = []
     for i in students:
         print('Введите сколькими языками владеет', i)
